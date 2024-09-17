c2aff12021834494d97dd63cc6712c59fa1a0f3d
Angi-Adema <dr.adema@gmail.com>
2024-09-17 12:47:07 -0600

    added CourseController class to handle the mapping of the list of classes to the browser and set up the REST API. Created the Course class to identify the attributes that will be printed to the screen when a user hits the /course url.

---
+++ src/main/java/com/in28minutes/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,30 @@
+package com.in28minutes.springboot.learn_spring_boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Create a simple REST API with the URL /courses.  http://localhost:8080/courses
+// When someone hits this URL, we want to return the below data back. Course: id, name, author
+//	[
+//	  {
+//		"id": 1,
+//		"name": "Learn Spring",
+//		"author": "in28minutes"
+//	  }
+//	]
+
+@RestController   // Must have RestController to build a Spring REST API.
+public class CourseController {
+	
+	// We need to map this so that when someone hits /courses, it returns this list of courses. (@RequestMapping("where mapping to"))
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses() {
+		return Arrays.asList(
+			new Course(1, "Learn Spring Boot", "in28minutes"),
+			new Course(2, "Learn Spring REST API", "in28minutes")
+			);
+	}
+}
